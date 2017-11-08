8e688a7e7e0675aac56f47da6b7384d40f6bb547
Mayra Dantas <mayradazevedo@gmail.com>
2017-11-08 20:15:11 -0300

    project vector orthogonal to list

---
+++ linear-algebra/src/main.py
@@ -0,0 +1,6 @@
+import projections
+
+v = [6,2]
+b = [2, 4]
+
+print( projections.project_orthogonal(b, v))
--- linear-algebra/src/projections.py
+++ linear-algebra/src/projections.py
@@ -1,13 +1,48 @@
 """
+Return the inner product of the vectors a and b
+"""
+def inner_product(a, b):
+    sum = 0.0;
+    for i in range(0,len(a)):
+        sum += a[i] * b[i]
+
+    return sum
+
+"""
+Calculate a - b
+"""
+def sub_vectors(a, b):
+    newList = [];
+    for i in range(0,len(b)):
+        newList.append(a[i] - b[i])
+
+    return newList
+
+"""
 Project vector b onto the span of v.
 """
 def project_along(b, v):
-    sigma = ((b*v)/(v*v)) if (v*v) != 0 else 0
-    return sigma*v
+    sigma = (inner_product(b,v)/inner_product(v,v)) if inner_product(v,v) != 0 else 0
+    newV = []
+    for i in range(0, len(v)):
+        newV.append(sigma * v[i])
+
+    return newV
 
 
 """
-Project vector b orthogonal to v.
+Project vector b orthogonal to vector v.
 """
+def project_orthogonal_vector(b, v):
+    return sub_vectors(b, project_along(b, v))
+
 def project_orthogonal(b, v):
-    return b - project_along(b, v);
+    """Project vector b orthogonal to list v.
+
+    Keyword arguments:
+    b -- the vector to be projected
+    v -- the list of mutually orthogonal vectors
+    """
+    for el in v:
+        b = sub_vectors(b, el)
+    return b
