e083930c87e5bdafaf9a29ccb03029a3cc586d42
Mayra Azevedo <mayradazevedo@gmail.com>
2018-03-05 13:41:56 -0300

    add s function

--- numerical-analysis/roots.cpp
+++ numerical-analysis/roots.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <cassert>
 
 //Questao 1
 float f (float x){
@@ -16,7 +17,7 @@ float f (float x){
 float bissecao( float(*func)(float ), float a, float b){
     float m = (a+b)/2.0;
 
-    for( int i =0; i < 10000; i++){
+    while( std::abs( 0.0 - func(m)) > std::pow(10, -5) ){
 
         if( func(a)*func(m) < 0){
             b = m;
@@ -34,7 +35,7 @@ float bissecao( float(*func)(float ), float a, float b){
 //Questao 2
 float g( float x){
     // f (x) =x 3 − 1.7x 2 − 12.78x − 10.08
-    return ((float) std::pow(x, (float)3) - (float) 1.7*std::pow(x, (float) 2) - (float) 12.78*x - (float) 10.08);
+    return ((float) std::pow(x, (float)3.0) - (float) 1.7*std::pow(x, (float) 2.0) - (float) 12.78*x - (float) 10.08);
 }
 
 float gPrime(float x){
@@ -42,16 +43,31 @@ float gPrime(float x){
     return ((float) 3.0 * std::pow(x, (float)2) - (float) 3.4*x - (float) 12.78);
 }
 
-float newton(float(*func)(float ), float(*func1)(float), float x){
+float newton(float(*func)(float ), float(*funcPrime)(float), float x){
     
     //xk+1 = xk - (fun(xk)/func1(xk))
-    auto xk = x - (func(x)/func1(x));
-    while( std::abs(xk - x)/std::abs(xk) > std::pow(10.0, -3.0) ){
+    auto xk = x - (func(x)/funcPrime(x));
+    while( std::abs(func(xk) - func(x)) > std::pow(10.0, -5.0) ){
         x = xk;
-        xk = x - (func(x)/func1(x));
+        xk = x - (func(x)/funcPrime(x));
     }
 
     return xk;
+}
+
+//Questao 3
+float s( float t ){
+    //s(t) = s0 − (mg/k) t + ((m^2*g) / (k^2))*(1-e^(-kt/m))
+    //g = 32.17, s0 = 300, m = 0.25, k = 0.1
+    float a = (float) (std::pow(0.25, 2.0)*32.17)/0.01;
+    float b = 1.0 - std::pow( (float) std::exp(1.0) , -0.4*t);
+
+    return (float)300.0 - (float) 80.425*t + (float) a*b;
+}
+
+float sPrime( float t) {
+
+   return  (-80.425 + 80.425 * std::pow(std::exp(1.0), (-0.4*t)));
 }
 
 int main(  ) {
@@ -73,6 +89,15 @@ int main(  ) {
     raiz = newton(g,gPrime, 3.0);
     std::cout << "Raiz: " << raiz << std::endl;
 
+    raiz = newton(s, sPrime, 7.06);
+    std::cout << raiz<< std::endl;
+    std::cout << s(raiz) <<std::endl;
+    assert( std::abs( 0.0 - s(raiz)) < std::pow(10, -5) );
+
+    raiz = bissecao(s, 5.05, 7.06);
+    std::cout << raiz<< std::endl;
+    assert( std::abs( 0.0 - s(raiz)) < std::pow(10, -5) );
+    std::cout << std::pow((float) 10.0, (float) -5.0) ;
 
     return EXIT_SUCCESS;
 }
