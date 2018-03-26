ab30e2f0d30712bf31825f42b381d83286b062c1
Mayra Azevedo <mayradazevedo@gmail.com>
2018-03-26 16:36:04 -0300

    add method for linear convolution

--- image-processing/src/image.py
+++ image-processing/src/image.py
@@ -1,7 +1,6 @@
-from scipy import ndimage, misc
 import numpy as np
 import cv2
-import os
+import convolution as conv
 
 face = cv2.imread('../data/dsc07348.jpg', cv2.IMREAD_COLOR)
 faceGray = cv2.imread('../data/dsc07348.jpg', cv2.IMREAD_GRAYSCALE)
@@ -29,8 +28,14 @@ img.append(cv2.filter2D(faceGray, ddepth, filterDetector))
 img.append(cv2.filter2D(face, ddepth, np.transpose(filterDetector)))
 img.append(cv2.filter2D(faceGray, ddepth, np.transpose(filterDetector)))
 
-import matplotlib.pyplot as plt
-
 n_images = len(img)
 for n, (image) in enumerate(img):
     cv2.imwrite('../gallery/image'+str(n)+'.png',image)
+    
+array1 = np.array([1,2,3,4,5])
+array2 = np.array([1,2,3])
+print(np.convolve(array1, array2, 'same'))
+print(np.convolve(array2, array1, 'same'))
+print(conv.linearConv(array1,array2, 'same'))
+print(np.convolve(array1, array2))
+print(conv.linearConv(array1,array2))
