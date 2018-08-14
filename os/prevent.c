b5dd61a1d389a2caa6864b2928923ce84d5ee7e2
Mayra Azevedo <mayradazevedo@gmail.com>
2018-08-13 21:38:48 -0300

    run ulimit -u

--- os/prevent.c
+++ os/prevent.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <stdint.h>
 
 int main(int argc, char **argv){
    int limit = atoi(argv[2]);
-   printf("%i", limit);
-   char buffer[20];
-   snprintf(buffer, sizeof(buffer), "getrlimit()");
-   system(buffer);
+   
+   // ulimit -u
+   struct rlimit rlim;
+   getrlimit(RLIMIT_NPROC, &rlim);
+   int64_t cur = rlim.rlim_cur;
+   int64_t max = rlim.rlim_max;
+   printf("RLIMIT :rlim_cur => %i, :rlim_max => %i", cur, max);
 }
+
