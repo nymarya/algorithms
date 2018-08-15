03bf47d4a0a8b25b97748feca9aaa419cf226c44
Mayra Azevedo <mayradazevedo@gmail.com>
2018-08-15 15:25:28 +0000

    Update prevent.c

--- os/prevent.c
+++ os/prevent.c
@@ -4,13 +4,33 @@
 #include <stdint.h>
 
 int main(int argc, char **argv){
-   int limit = atoi(argv[2]);
-   
-   // ulimit -u
-   struct rlimit rlim;
-   getrlimit(RLIMIT_NPROC, &rlim);
-   int64_t cur = rlim.rlim_cur;
-   int64_t max = rlim.rlim_max;
-   printf("RLIMIT :rlim_cur => %i, :rlim_max => %i", cur, max);
-}
+    if(argc>1){
+        int limit = atoi(argv[2]);
 
+	
+
+	struct rlimit rlim;
+        getrlimit(RLIMIT_NPROC, &rlim);
+        int cur = rlim.rlim_cur;
+     	int max = rlim.rlim_max;
+	printf("RLIMIT :rlim_cur => %i, :rlim_max => %i", cur, max);
+
+	// Change the limit 
+	rlim.rlim_cur = limit; 
+	rlim.rlim_max = limit; 
+
+	// Now call setrlimit() to set the  
+	// changed value. 
+	setrlimit (RLIMIT_NPROC, &rlim);
+
+	// Check
+	getrlimit(RLIMIT_NPROC, &rlim);
+	cur = rlim.rlim_cur;
+     	max = rlim.rlim_max;
+	printf("\nRLIMIT :rlim_cur => %i, :rlim_max => %i", cur, max); 
+
+    }
+   
+   // ulimit -u $limit
+
+}
