505e83446c862154cecb066e4318e946520ec33d
Mayra Azevedo <mayradazevedo@gmail.com>
2018-09-03 09:00:13 -0300

    add program for consuming cpu

---
+++ os/trabalho1.2/mult.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#define N 700
+ 
+// This function multiplies mat1[][] and mat2[][],
+// and stores the result in res[][]
+void multiply(int mat1[][N], int mat2[][N], int res[][N])
+{
+    int i, j, k;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            res[i][j] = 0;
+            for (k = 0; k < N; k++)
+                res[i][j] += mat1[i][k]*mat2[k][j];
+        }
+    }
+}
+ 
+int main()
+{
+    int mat1[N][N];
+ 
+    int mat2[N][N];
+
+    for ( int x = 0; x < 10; x--){
+        for (int i =0; i< N; i++){
+            for(int j = 0; j< N; j++){
+                mat1[i][j] = i + j + x;
+                mat2[i][j] = i * j * x;
+            }
+        }
+    
+        int res[N][N]; // To store result
+        int i, j;
+        multiply(mat1, mat2, res);
+        sleep(1);
+    }
+ 
+    return 0;
+}
