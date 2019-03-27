3bad600f56c828d4882004d70de0c8e2c38b11f6
Mayra Azevedo <mayradazevedo@gmail.com>
2019-03-27 15:14:25 -0300

    Create classical.lean

---
+++ lean_exercises/classical.lean
@@ -0,0 +1,47 @@
+open classical
+
+variables p q r s : Prop
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := 
+iff.intro(
+    assume h1: p ∧ q,
+    show q ∧ p, from and.intro( and.right h1 ) ( and.left h1)
+)
+(
+    assume h: q ∧ p,
+    show p ∧ q, from and.intro( and.right h)(and.left h)
+) --end proof
+
+example : p ∨ q ↔ q ∨ p := sorry
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ false ↔ p := sorry
+example : p ∧ false ↔ false := sorry
+example : ¬(p ↔ ¬p) := sorry
+example : (p → q) → (¬q → ¬p) := sorry
+
+-- these require classical reasoning
+example : (p → r ∨ s) → ((p → r) ∨ (p → s)) := sorry
+example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
+example : ¬(p → q) → p ∧ ¬q := sorry
+example : (p → q) → (¬p ∨ q) := sorry
+example : (¬q → ¬p) → (p → q) := sorry
+example : p ∨ ¬p := sorry
+example : (((p → q) → p) → p) := sorry
