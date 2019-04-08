d9d842394fa5e0e992ec58ec2635eaece12df79f
Mayra Azevedo <mayradazevedo@gmail.com>
2019-04-08 09:01:51 -0300

    Update ex1.lean

--- lean_exercises/ex1.lean
+++ lean_exercises/ex1.lean
@@ -6,15 +6,12 @@ variables x y z a: α
 variable r: Prop
 
 
-example : (∀ x, p x → q x ) →   (∃ y, p y) → (∃ z , q z) :=
-assume h : (∀ x, p x → q x ),
-    show (∃ y, p y) → (∃ z , q z), from
-    (assume h2 : ∃ y, p y,
-        show (∃ z , q z), from
-        exists.elim h2 
-        (
-            assume w: α, 
-            assume hw: p w, 
-            exists.intro (w) ( h(w)(hw) ) 
-        )
+example (h : ∀ x, p x → q x ) :  (∃ y, p y) → (∃ z , q z) :=
+assume h1: ∃ y, p y,
+show (∃ z , q z), from
+    exists.elim h1 
+    (
+        assume w: α, 
+        assume hw: p w, 
+        exists.intro (w) ( h(w)(hw) ) 
     )
